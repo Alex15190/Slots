1f9193aac0fa1d1eb43d988667fff37a26bf5504
Alex Chekodanov <checkodanov@gmail.com>
2021-12-03 13:47:23 +0300

    Created small app

---
+++ Slots/Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Alex Chekodanov on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
